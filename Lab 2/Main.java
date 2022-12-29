86def9e362618dba5e02ef1e6dfb57cc3073225b
ys-niloy <115401989+ys-niloy@users.noreply.github.com>
2022-12-29 20:42:51 +0600

    Add files via upload

---
+++ Lab 2/Main.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+
+public class Main{
+
+    public static void main(String[] args)
+    {
+          
+            Book b1 = new Book("1-22-31", "The Silent Patient", "Mike", 750, 30);
+
+
+        b1.showDetails();
+        
+
+
+/*
+        System.out.println("Book Isbn            : "+b1.getIsbn());
+        System.out.println("Book Title           :"+b1.getBookTitle());
+        System.out.println("Author Name          :"+b1.getAuthorName());
+        System.out.println("Price                :"+b1.getPrice());
+        System.out.println("Available Quantity   :"+b1.getAvailableQuantity());
+*/
+    
+
+
+    }
+}
