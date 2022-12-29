1e53aeb6762c8434b5cdde7164df723d848b5486
ys-niloy <115401989+ys-niloy@users.noreply.github.com>
2022-12-29 14:50:14 +0600

    Add files via upload

---
+++ Lab 1/B/Account.java
@@ -0,0 +1,71 @@
+import java.lang.*;
+
+public class Account
+{
+	private String id;
+	private String name;
+	private double balance;
+	
+	public Account()
+	{
+		System.out.println("Default");
+	}
+	
+	public Account(String id,String name,double balance)
+	{
+		System.out.println("Parameterized");
+		this.id=id;
+		this.name=name;
+		this.balance=balance;
+	}
+		
+	public void setValues(String id,String name,double balance)
+	{
+		this.id=id;
+		this.name=name;
+		this.balance=balance;
+	}
+	
+	public String getId()
+	{
+		return this.id;
+	}
+	public String getName()
+	{
+		return this.id;
+	}
+	public double getBalance()
+	{
+		return this.balance;
+	}
+	
+	public void deposit(double amount)
+	{
+		if(amount>0)
+		{
+			this.balance+=amount;
+		}
+	}
+	public void withdraw(double amount)
+	{
+		if(balance>=amount && amount>0)
+		{
+			this.balance-=amount;
+		}
+	}
+
+	/*public void transfer(Account object,double amount)
+	{
+		this.withdraw(amount);
+		object.deposit(amount);
+		
+	}*/
+
+	public void show()
+	{
+		System.out.println("Account Holder 		:"+this.name);
+		System.out.println("ID 			   		:"+this.id);
+		System.out.println("Balance 	   		:"+this.balance);
+	}
+	
+}
---
+++ Lab 1/B/Main.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Account a1 = new Account("111", "Mr. Y", 30000);
+
+		a1.show();
+	}
+}
